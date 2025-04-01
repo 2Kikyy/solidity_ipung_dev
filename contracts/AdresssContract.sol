f960eb91aa4d0ff189fc9974bdd9c5cd6a157fdf
2Kikyy <kiky.794jr@gmail.com>
2025-04-01 18:20:29 +0700

    Belajar address & data locations

---
+++ contracts/AdresssContract.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+contract AddressContract {
+    // address = nomor rekening
+    // smart contract punya address
+
+    // address payable 
+    // send dan transfer ether
+
+    // address public caller;
+
+    // function getCallerAddress() public returns(address) {
+    //     caller = msg.sender;
+
+    //     return caller;
+    // }
+
+    // function getCallerAddress() public view returns(address caller) {
+    //     caller = msg.sender;
+    // }
+
+    uint receivedAmount;
+
+    function getAddress() public view returns(address) {
+        address myAddress = address(this);
+        return myAddress;
+    }
+
+    function receivedEther() payable public {
+        receivedAmount = msg.value;
+    }
+
+    //address payable
+    function transferFund(address payable _address, uint nominal) public {
+        _address.transfer(nominal);
+    }
+
+    function sendFund(address payable _address, uint nominal) public returns(bool) {
+        _address.send(nominal);
+    }
+}
---
+++ contracts/Location.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+// lokasi data
+// gas
+
+//  storage
+// memory
+// calldata
+// stack
